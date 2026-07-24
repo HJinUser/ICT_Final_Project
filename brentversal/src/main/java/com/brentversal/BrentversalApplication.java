5425ef5d858b13654883118a6d64e32571c4f769
Hyeonmin Yi <rktngusals@gmail.com>
2026-07-24 18:35:46 +0900

    first

---
+++ brentversal/src/main/java/com/brentversal/BrentversalApplication.java
@@ -0,0 +1,13 @@
+package com.brentversal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrentversalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrentversalApplication.class, args);
+	}
+
+}
---
+++ brentversal/src/test/java/com/brentversal/BrentversalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brentversal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrentversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
