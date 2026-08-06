c0422a0d25429dee391fd413c178f7b720063208
JooJongChan <yalejong96@gmail.com>
2026-08-06 15:05:12 +0900

    propertydetail 작성

--- frentversal/src/routes/AppRoutes.tsx
+++ frentversal/src/routes/AppRoutes.tsx
@@ -5,6 +5,8 @@ import LoginPage from './../pages/LoginPage';
 import HomePage from './../pages/HomePage';
 import PropertyFormPage from './../pages/PropertyFormPage';
 import type { User } from "../types/User";
+import PropertyPage from './../pages/PropertyPage';
+import PropertyPreviewPage from "../pages/PropertyPreviewPage";
 
 
 interface AppProps {
@@ -12,7 +14,7 @@ interface AppProps {
   handleLoginSuccess: (userData: User) => void;
 }
 
-function App({ handleLoginSuccess }: AppProps) {
+function App({ user, handleLoginSuccess }: AppProps) {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
@@ -20,6 +22,8 @@ function App({ handleLoginSuccess }: AppProps) {
       <Route path='/member/signup' element={<SignupPage />} />
       <Route path='/member/login' element={<LoginPage onLogin={handleLoginSuccess} />} />
       <Route path='/property/form' element={<PropertyFormPage />} />
+      <Route path="/property/:id" element={<PropertyPage user={user} />} />
+      <Route path="/property/preview" element={<PropertyPreviewPage />} />
     </Routes>
   );
 }
