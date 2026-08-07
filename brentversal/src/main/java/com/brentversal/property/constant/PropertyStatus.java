5c79bbc33edadacbaff83374c5deafc2ff556d14
JooJongChan <yalejong96@gmail.com>
2026-08-07 18:27:30 +0900

    brentversal-property 작성

--- brentversal/src/main/java/com/brentversal/property/constant/PropertyStatus.java
+++ brentversal/src/main/java/com/brentversal/property/constant/PropertyStatus.java
@@ -1,9 +1,9 @@
 package com.brentversal.property.constant;
 
 public enum PropertyStatus {
+    PENDING,        // 승인대기 (신규 등록 시 기본값, 관리자 승인 전)
     ACTIVE,         // 게시중
     IN_PROGRESS,    // 거래진행중
     COMPLETED,      // 거래완료 (되돌릴 수 없음)
-    CANCELLED,      // 등록취소 (되돌릴 수 없음)
-    HIDDEN          // 비공개
+    CANCELLED       // 등록취소 (되돌릴 수 없음)
 }
